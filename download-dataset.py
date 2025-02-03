34569e8f7805c0ace0dcfec0114b9820b4a881f5
Artem <artdoctorka@gmail.com>
2025-02-03 22:29:15 +0100

    update dataset, gitignore

--- download-dataset.py
+++ download-dataset.py
@@ -1,7 +1,6 @@
 from roboflow import Roboflow
-import os
 
-rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
-project = rf.workspace("ashspace-7icj2").project("lethal-yolo")
-version = project.version(1)
+rf = Roboflow(api_key="V8hkEWY7apLYPHW6Mk7F")
+project = rf.workspace("ashspace-7icj2").project("lethal-company-object-detection-v2")
+version = project.version(2)
 dataset = version.download("yolov11")
