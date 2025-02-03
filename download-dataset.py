6fb0b99426a686d5c8a3cb3cb3cb29b2dfa3b793
Artem <artdoctorka@gmail.com>
2025-02-03 22:46:04 +0100

    remove api key i forgor

--- download-dataset.py
+++ download-dataset.py
@@ -1,6 +1,7 @@
+import os
 from roboflow import Roboflow
 
-rf = Roboflow(api_key="V8hkEWY7apLYPHW6Mk7F")
+rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
 project = rf.workspace("ashspace-7icj2").project("lethal-company-object-detection-v2")
 version = project.version(2)
 dataset = version.download("yolov11")
